387213d5918bd6c4ba7886271376d805c3f7a223
lunaxlam@gmail.com <lunalam@Lunas-MBP.attlocal.net>
2022-04-07 17:45:47 -0700

    Completed assignment

---
+++ melons.py
@@ -0,0 +1,40 @@
+import robots
+
+
+class Melon:
+    """Melon."""
+
+    def __init__(self, melon_type):
+        """Initialize melon.
+
+        melon_type: type of melon being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return f"{self.color} {self.weight:.2f} lbs {self.melon_type}"
+
+
+class Squash(Melon):
+    """Squash melon."""
+
+    def prep(self):
+
+        # Inherits the prep method from the parent class
+        super().prep()
+        robots.painterbot.paint(self)
